341dd37aac0ba88c8330ba6454a06c46a4d7baa8
Akshay Patil <akpatilm@amazon.com>
2026-03-12 23:40:32 -0400

    Used bs4 to read from URL

--- main.py
+++ main.py
@@ -1,36 +1,55 @@
 import getpass
 import os
 import faiss
+import bs4
+from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.docstore.in_memory import InMemoryDocstore
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
-os.environ["LANGSMITH_TRACING"] = "true"
-os.environ["LANGSMITH_API_KEY"] = getpass.getpass()
-if not os.environ.get("GOOGLE_API_KEY"):
-    os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
 
-# Google Gemini chat model
-model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
+def setup_components():
+    os.environ["LANGSMITH_TRACING"] = "true"
+    os.environ["LANGSMITH_API_KEgY"] = getpass.getpass()
+    if not os.environ.get("GOOGLE_API_KEY"):
+        os.environ["GOOGLE_API_KEY"] = getpass.getpass(
+            "Enter API key for Google Gemini: "
+        )
 
-# Google Gemini embeddings model
-embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
+    # Google Gemini chat model
+    model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
 
-# FAISS vector store
-embedding_dim = len(embeddings.embed_query("hello world"))
-index = faiss.IndexFlatL2(embedding_dim)
+    # Google Gemini embeddings model
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
 
-vector_store = FAISS(
-    embedding_function=embeddings,
-    index=index,
-    docstore=InMemoryDocstore(),
-    index_to_docstore_id={},
-)
+    # FAISS vector store
+    embedding_dim = len(embeddings.embed_query("hello world"))
+    index = faiss.IndexFlatL2(embedding_dim)
+
+    vector_store = FAISS(
+        embedding_function=embeddings,
+        index=index,
+        docstore=InMemoryDocstore(),
+        index_to_docstore_id={},
+    )
 
 
 def main():
     print("Hello from rag-agent!")
+    # Only keep post title, headers, and content from the full HTML.
+    bs4_strainer = bs4.SoupStrainer(
+        class_=("post-title", "post-header", "post-content")
+    )
+    loader = WebBaseLoader(
+        web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+        bs_kwargs={"parse_only": bs4_strainer},
+    )
+    docs = loader.load()
+
+    assert len(docs) == 1
+    print(f"Total characters: {len(docs[0].page_content)}")
+    print(docs[0].page_content[:500])
 
 
 if __name__ == "__main__":
